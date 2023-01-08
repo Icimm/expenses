e60d8f53fbb5885f6e5e5498e732bb35608b9e2c
icaro <icarobellemskr@hotmail.com>
2023-01-08 17:02:32 -0300

    criacao da aplicacao
    estilizacao
    
    funcoes e classes
    
    importacao de pacote inml
    
    estilizção e criação de formulario
    
    separção da arvore de widgets em components
    
    criação de novos components

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:expenses/models/transactions.dart';
+
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+import 'components/transaction.user.dart';
 
 main() => runApp(ExpensesApp());
 
@@ -28,23 +28,6 @@ class MyhomePage extends StatefulWidget {
 class _MyhomePageState extends State<MyhomePage> {
 
 
-  final _transactions = [
-    Transaction(
-        id: 't1',
-        title: 'Novo Tênis de Skate',
-        value: 310.76,
-        date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Conta de Luz',
-      value: 211.30,
-      date: DateTime.now(),
-    ),
-  ];
-
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -60,82 +43,7 @@ class _MyhomePageState extends State<MyhomePage> {
               elevation: 5,
             ),
           ),
-          Column(
-            children: _transactions.map((tr) {
-              return Card(
-                 child: Row(
-                   children: <Widget>[
-                     Container(
-                       margin: EdgeInsets.symmetric(
-                         horizontal: 15,
-                             vertical: 10,
-                       ),
-                       decoration: BoxDecoration(
-                         border: Border.all(
-                           color: Colors.purple,
-                           width: 2,
-                         ),
-                       ),
-                       padding: EdgeInsets.all(10),
-                       child: Text(
-                         'R\$ ${tr.value.toStringAsFixed(2)}',
-                             style: TextStyle(
-                             fontWeight: FontWeight.bold,
-                               fontSize: 20,
-                               color: Colors.purple,
-                        ),
-                       ),
-                     ),
-                     Column(
-                         crossAxisAlignment: CrossAxisAlignment.start,
-                         children: <Widget>[
-                           Text(
-                               tr.title,
-                               style: TextStyle(
-                                 fontWeight: FontWeight.bold,
-                                 fontSize: 16,
-                               ),
-                           ),
-                               Text(
-                                   DateFormat('d MMM y').format(tr.date),
-                                   style: TextStyle(
-                                     color: Colors.grey,
-                                   ),
-                                ),
-                             ],
-                     ),
-                   ],
-                 ),
-              );
-            }).toList()
-          ),
-          Card(
-            elevation: 5,
-            child: Padding(
-              padding: const EdgeInsets.all(10),
-              child: Column(
-                children: <Widget>[
-                  TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Titulo',
-                    ),
-                  ),
-                  TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Valor (R\$)',
-                    ),
-                  ),
-                  TextButton(
-                    child: Text('Nova Transação'),
-                    style: ButtonStyle(
-                      foregroundColor: MaterialStateProperty.all<Color>(Colors.purple),
-                    ),
-                    onPressed: () {},
-                  )
-                ],
-              ),
-            ),
-          )
+         TransactionUser(),
         ],
       ),
     );
