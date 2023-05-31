8051762a7b66744176f62983f429f8a761f0b80a
Icimm <icarobellemskr@hotmail.com>
2023-05-31 11:36:37 -0300

    new file:   .vscode/launch.json 	modified:   lib/components/chart.dart

--- lib/components/chart.dart
+++ lib/components/chart.dart
@@ -4,31 +4,30 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 class Chart extends StatelessWidget {
-
   final List<Transaction> recentTransaction;
 
   Chart(this.recentTransaction);
 
   List<Map<String, dynamic>> get groupedTransactions {
     return List.generate(7, (index) {
-     final weekDay = DateTime.now().subtract(
-       Duration(days: index),
-     );
+      final weekDay = DateTime.now().subtract(
+        Duration(days: index),
+      );
 
       double totalSum = 0.0;
 
-      for(var i = 0; i < recentTransaction.length; i++) {
-       bool sameDay = recentTransaction[i].date.day == weekDay.day;
-       bool sameMonth = recentTransaction[i].date.month == weekDay.month;
-       bool sameYear = recentTransaction[i].date.year == weekDay.year;
+      for (var i = 0; i < recentTransaction.length; i++) {
+        bool sameDay = recentTransaction[i].date.day == weekDay.day;
+        bool sameMonth = recentTransaction[i].date.month == weekDay.month;
+        bool sameYear = recentTransaction[i].date.year == weekDay.year;
 
-       if(sameDay && sameMonth && sameYear) {
-         totalSum += recentTransaction[i].value;
-       }
+        if (sameDay && sameMonth && sameYear) {
+          totalSum += recentTransaction[i].value;
+        }
       }
 
-      return{
-        'day':DateFormat.E().format(weekDay)[0],
+      return {
+        'day': DateFormat.E().format(weekDay)[0],
         'value': totalSum,
       };
     }).reversed.toList();
@@ -37,13 +36,11 @@ class Chart extends StatelessWidget {
   double get _weekTotalValue {
     return groupedTransactions.fold(0.0, (sum, tr) {
       return sum + tr['value'];
-
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    groupedTransactions;
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
@@ -51,16 +48,18 @@ class Chart extends StatelessWidget {
         padding: const EdgeInsets.all(10),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children:groupedTransactions.map((tr) {
-             return Flexible(
-               fit: FlexFit.tight,
-               child: ChartBar(
-               label: tr['day'],
-               value: tr['value'],
-               percentage: _weekTotalValue == 0 ? 0 : (tr['value'] as double) / _weekTotalValue,
-             ),
-           );
-         }).toList(),
+          children: groupedTransactions.map((tr) {
+            return Flexible(
+              fit: FlexFit.tight,
+              child: ChartBar(
+                label: tr['day'],
+                value: tr['value'],
+                percentage: _weekTotalValue == 0
+                    ? 0
+                    : (tr['value'] as double) / _weekTotalValue,
+              ),
+            );
+          }).toList(),
         ),
       ),
     );
